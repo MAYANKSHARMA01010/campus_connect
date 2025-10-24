951f1340efc5968957d05aa59b2ead01daa083c2
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-10-24 18:33:55 +0530

    4 screns added

---
+++ Screens/Events.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    View,
+    Text
+} from 'react-native'
+
+function Events() {
+  return (
+    <View>
+        <Text>
+            Events Screen
+        </Text>
+    </View>
+  )
+}
+
+export default Events
---
+++ Screens/Search.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    View,
+    Text
+} from 'react-native'
+
+function Search() {
+  return (
+    <View>
+        <Text>
+            Search Screen
+        </Text>
+    </View>
+  )
+}
+
+export default Search
