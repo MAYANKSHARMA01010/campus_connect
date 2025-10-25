4d9c81d5f98945e62e58f2c8bfab75e30ffa45ad
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-10-25 14:16:34 +0530

    icons added in all navigations

--- components/BottomNavigationMainScreen.jsx
+++ components/BottomNavigationMainScreen.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { 
+  Entypo,
+  Ionicons,
+  MaterialIcons,
+  FontAwesome6,
+} from '@expo/vector-icons'; 
 
 import HomeScreen from '../Screens/Home';
 import EventScreen from '../Screens/Events';
@@ -11,10 +17,39 @@ const Tabs = createBottomTabNavigator();
 export default function BottomNavigationMainScreen() {
   return (
     <Tabs.Navigator>
-      <Tabs.Screen name="Home" component={HomeScreen} />
-      <Tabs.Screen name="Search" component={SearchScreen} />
-      <Tabs.Screen name="Events" component={EventScreen} />
-      <Tabs.Screen name="Profile" component={ProfileScreen} />
+
+      <Tabs.Screen
+        options={{
+          tabBarIcon: () => <Entypo name="home" size={24} color="black" />,
+          tabBarIconStyle: { marginRight: 2 },
+        }}
+        name="Home" component={HomeScreen} 
+      />
+
+      <Tabs.Screen
+        options={{
+          tabBarIcon: () => <Ionicons name="search" size={24} color="black" />,
+          tabBarIconStyle: { marginRight: 3 },
+        }}
+        name="Search" component={SearchScreen} 
+      />
+
+      <Tabs.Screen 
+        options={{
+          tabBarIcon: () => <MaterialIcons name="event" size={24} color="black" />,
+          tabBarIconStyle: { marginRight: 2 },
+        }}
+        name="Events" component={EventScreen} 
+      />
+
+      <Tabs.Screen
+        options={{
+          tabBarIcon: () => <FontAwesome6 name="user" size={24} color="black" />,
+          tabBarIconStyle: { marginRight: 2 },
+        }}
+        name="Profile" component={ProfileScreen} 
+      />
+
     </Tabs.Navigator>
   );
 }
