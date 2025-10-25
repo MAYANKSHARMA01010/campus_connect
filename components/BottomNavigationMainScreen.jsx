d419bc9dbfdbc54b81ecfe9c52820d6ed47b6e50
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-10-25 15:28:50 +0530

    updated  header in navigation

--- components/BottomNavigationMainScreen.jsx
+++ components/BottomNavigationMainScreen.jsx
@@ -6,6 +6,11 @@ import {
   MaterialIcons,
   FontAwesome6,
 } from '@expo/vector-icons'; 
+import { 
+  Image, 
+  Text, 
+  StyleSheet 
+} from 'react-native';
 
 import HomeScreen from '../Screens/Home';
 import EventScreen from '../Screens/Events';
@@ -16,40 +21,70 @@ const Tabs = createBottomTabNavigator();
 
 export default function BottomNavigationMainScreen() {
   return (
-    <Tabs.Navigator>
-
+    <Tabs.Navigator
+      initialRouteName='Home'
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: 'transparent',
+          elevation: 0,
+          shadowOpacity: 0,
+        },
+        headerTitleAlign: 'center',
+      }}
+    >
       <Tabs.Screen
+        name="Home"
+        component={HomeScreen}
         options={{
           tabBarIcon: () => <Entypo name="home" size={24} color="black" />,
-          tabBarIconStyle: { marginRight: 2 },
+          headerTitle: () => <Text style={styles.headerTitleText}>Home</Text>,
+          headerLeft: () => <Image source={require("../assets/LOGO.png")} style={styles.headerLogo} />,
         }}
-        name="Home" component={HomeScreen} 
       />
 
       <Tabs.Screen
+        name="Search"
+        component={SearchScreen}
         options={{
           tabBarIcon: () => <Ionicons name="search" size={24} color="black" />,
-          tabBarIconStyle: { marginRight: 3 },
+          headerTitle: () => <Text style={styles.headerTitleText}>Search</Text>,
+          headerLeft: () => <Image source={require("../assets/LOGO.png")} style={styles.headerLogo} />,
         }}
-        name="Search" component={SearchScreen} 
       />
 
       <Tabs.Screen 
+        name="Events"
+        component={EventScreen} 
         options={{
           tabBarIcon: () => <MaterialIcons name="event" size={24} color="black" />,
-          tabBarIconStyle: { marginRight: 2 },
+          headerTitle: () => <Text style={styles.headerTitleText}>Events</Text>,
+          headerLeft: () => <Image source={require("../assets/LOGO.png")} style={styles.headerLogo} />,
         }}
-        name="Events" component={EventScreen} 
       />
 
       <Tabs.Screen
+        name="Profile"
+        component={ProfileScreen} 
         options={{
           tabBarIcon: () => <FontAwesome6 name="user" size={24} color="black" />,
-          tabBarIconStyle: { marginRight: 2 },
+          headerTitle: () => <Text style={styles.headerTitleText}>Profile</Text>,
+          headerLeft: () => <Image source={require("../assets/LOGO.png")} style={styles.headerLogo} />,
         }}
-        name="Profile" component={ProfileScreen} 
       />
-
     </Tabs.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  headerLogo: {
+    width: 40,
+    height: 40,
+    resizeMode: 'contain',
+    marginLeft: 10,
+  },
+  headerTitleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
