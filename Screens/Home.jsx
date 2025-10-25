ded1e391966b811d394eb42eb274979c98b17d7b
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-10-25 19:06:36 +0530

    title added

--- Screens/Home.jsx
+++ Screens/Home.jsx
@@ -1,24 +1,46 @@
-import React from 'react'
-import {
-    View,
-    Text,
-    StyleSheet,
-} from 'react-native'
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.mainContainer}>
-        <Text>
-            Home Screen
-        </Text>
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <Text style={styles.bigC}>C</Text>
+
+        <View style={styles.rightTextContainer}>
+          <Text style={styles.ampus}>AMPUS</Text>
+          <Text style={styles.onnect}>ONNECT</Text>
+        </View>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center'
+  container: {
+    marginTop: 18,
+    marginLeft: 38,
   },
-})
+  row: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+  },
+  bigC: {
+    fontSize: 60,
+    fontWeight: 'bold',
+  },
+  rightTextContainer: {
+    flexDirection: 'column',
+    marginTop: 16,
+    marginLeft: 3,
+  },
+  ampus: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  onnect: {
+    fontSize: 20,
+    marginTop: -5,
+    fontWeight: 'bold',
+  },
+});
