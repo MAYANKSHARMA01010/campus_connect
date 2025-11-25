17ed0d2deba531d058c8f54ec3471e1a8cbfc1fb
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-11-25 23:48:23 +0530

    event router added in index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const corsMiddleware = require("./config/cors.js");
 const authRouter = require("./routes/userRoute");
+const eventRouter = require("./routes/eventRoute.js");
 require("dotenv").config();
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(corsMiddleware);
 app.use(express.json());
 
 app.use("/api/auth", authRouter)
+app.use("/api/events", eventRouter);
 
 app.get("/", (req, res) => {
   res.status(200).send("<h1>Backend Running Successfully 🚀</h1>");
