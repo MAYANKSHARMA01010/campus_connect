9e2373ca0d9679f80ea85097266e91b53243f607
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-11-26 13:30:28 +0530

    db error fixed done

--- config/cors.js
+++ config/cors.js
@@ -1,25 +1,33 @@
+// const cors = require("cors");
+// require("dotenv").config();
+
+// const allowedOrigins = [
+//     process.env.FRONTEND_LOCAL_URL,
+//     process.env.FRONTEND_SERVER_URL,
+//     null,
+// ];
+
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             return callback(null, true);
+//         }
+
+//         console.warn(`❌ Blocked by CORS: ${origin}`);
+//         return callback(new Error("Not allowed by CORS"));
+//     },
+
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+// };
+
+// module.exports = cors(corsOptions);
+
 const cors = require("cors");
-require("dotenv").config();
 
-const allowedOrigins = [
-    process.env.FRONTEND_LOCAL_URL,
-    process.env.FRONTEND_SERVER_URL,
-    null,
-];
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        }
-
-        console.warn(`❌ Blocked by CORS: ${origin}`);
-        return callback(new Error("Not allowed by CORS"));
-    },
-
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-module.exports = cors(corsOptions);
+module.exports = cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+});
